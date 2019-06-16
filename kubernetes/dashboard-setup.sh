cc055553b52429c0d10a405fbb362e6bc930f0a6
yafernandes <alexandrino.fernandes@gmail.com>
2019-06-16 12:24:19 -0400

    Ansible now uses roles

--- kubernetes/dashboard-setup.sh
+++ kubernetes/dashboard-setup.sh
@@ -1,2 +1,4 @@
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
 kubectl apply -f dashboard.yaml
 kubectl config set-credentials kubernetes-admin --token=$(kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-admin | awk '{print $1}') | grep ^token: | awk '{print $2}')
