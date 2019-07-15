a2fb2317e633e8bf35868829ca398e2f6565b711
yafernandes <alexandrino.fernandes@gmail.com>
2019-07-15 10:52:01 -0400

    Fixed typo

--- kubernetes/dashboard-setup.sh
+++ kubernetes/dashboard-setup.sh
@@ -1,3 +1,3 @@
-kubectl apply -f hhttps://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta1/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta1/aio/deploy/recommended.yaml
 kubectl apply -f dashboard.yaml
 kubectl config set-credentials kubernetes-admin --token=$(kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-admin | awk '{print $1}') | grep ^token: | awk '{print $2}')
