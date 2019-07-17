783c007cd894755534748e5b3602158b3f7b0b6d
yafernandes <alexandrino.fernandes@gmail.com>
2019-07-16 20:11:41 -0400

    Generates inventory.txt

--- build.sh
+++ build.sh
@@ -3,6 +3,18 @@ LOC=$(cd `dirname $0` && pwd)
 cd ${LOC}/terraform
 terraform apply --auto-approve
 
+echo master ansible_host=master.k8s.aws.pipsquack.ca ansible_connection=ssh ansible_user=centos ansible_ssh_private_key_file=/Users/alex.fernandes/.ssh/alexf-key.pem > ${LOC}/ansible/inventory.txt
+for ENTRY in $(terraform output -json workers | jq --raw-output '.[]')
+do
+    echo $(echo $ENTRY | cut -f 1 -d .) ansible_host=$ENTRY ansible_connection=ssh ansible_user=centos ansible_ssh_private_key_file=/Users/alex.fernandes/.ssh/alexf-key.pem >> ${LOC}/ansible/inventory.txt
+done
+
+echo [workers] >> ${LOC}/ansible/inventory.txt
+for ENTRY in $(terraform output -json workers | jq --raw-output '.[]')
+do
+    echo $(echo $ENTRY | cut -f 1 -d .) >> ${LOC}/ansible/inventory.txt
+done
+
 cd ${LOC}/ansible
 ansible-playbook -i inventory.txt main.yaml
 
