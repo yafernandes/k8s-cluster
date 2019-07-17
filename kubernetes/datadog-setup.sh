52454f6e60237f865b4c3fcfd1a94d0373165867
yafernandes <alexandrino.fernandes@gmail.com>
2019-07-16 20:14:14 -0400

    Istio support

--- kubernetes/datadog-setup.sh
+++ kubernetes/datadog-setup.sh
@@ -1,11 +1,16 @@
 # https://docs.datadoghq.com/agent/kubernetes/daemonset_setup/
-kubectl apply -f "https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/clusterrole.yaml"
-kubectl apply -f "https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/serviceaccount.yaml"
-kubectl apply -f "https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/clusterrolebinding.yaml"
+kubectl apply -f https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/clusterrole.yaml
+kubectl apply -f https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/serviceaccount.yaml
+kubectl apply -f https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/clusterrolebinding.yaml
+
+kubectl apply -f https://github.com/DataDog/datadog-agent/raw/master/Dockerfiles/manifests/cluster-agent/rbac/rbac-cluster-agent.yaml
+
 
 # Kubernetes Event Collection
 # https://docs.datadoghq.com/agent/kubernetes/event_collection/
 kubectl create configmap datadogtoken --from-literal="event.tokenKey"="0"
+
+kubectl create configmap dd-conf  --from-file istio.yaml
 
 kubectl create secret generic dd --from-env-file=secrets.txt
 
@@ -13,4 +18,4 @@ kubectl create secret generic dd --from-env-file=secrets.txt
 # https://docs.datadoghq.com/agent/kubernetes/host_setup/
 kubectl apply -f kube-state-metrics
 
-kubectl apply -f datadog-agent.yaml
+kubectl apply -f datadog-cluster-agent.yaml
