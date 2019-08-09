d8acb549208c25277e05c4187c2f34c6a5595164
yafernandes <alexandrino.fernandes@gmail.com>
2019-08-09 11:34:57 -0400

    Moved username to variable

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 LOC=$(cd `dirname $0` && pwd)
+USERNAME=admin
 
 cd ${LOC}/terraform
 terraform apply --auto-approve
 
-echo master ansible_host=master.k8s.aws.pipsquack.ca ansible_connection=ssh ansible_user=centos ansible_ssh_private_key_file=/Users/alex.fernandes/.ssh/alexf-key.pem > ${LOC}/ansible/inventory.txt
+echo master ansible_host=master.k8s.aws.pipsquack.ca ansible_connection=ssh ansible_user=$USERNAME ansible_ssh_private_key_file=/Users/alex.fernandes/.ssh/alexf-key.pem > ${LOC}/ansible/inventory.txt
 for ENTRY in $(terraform output -json workers | jq --raw-output '.[]')
 do
-    echo $(echo $ENTRY | cut -f 1 -d .) ansible_host=$ENTRY ansible_connection=ssh ansible_user=centos ansible_ssh_private_key_file=/Users/alex.fernandes/.ssh/alexf-key.pem >> ${LOC}/ansible/inventory.txt
+    echo $(echo $ENTRY | cut -f 1 -d .) ansible_host=$ENTRY ansible_connection=ssh ansible_user=$USERNAME ansible_ssh_private_key_file=/Users/alex.fernandes/.ssh/alexf-key.pem >> ${LOC}/ansible/inventory.txt
 done
 
 echo [workers] >> ${LOC}/ansible/inventory.txt
