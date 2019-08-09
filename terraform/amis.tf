b908bdceb5058e0936a233186446769ebb2c7d17
yafernandes <alexandrino.fernandes@gmail.com>
2019-08-09 11:34:27 -0400

    Added new images

--- terraform/amis.tf
+++ terraform/amis.tf
@@ -22,3 +22,78 @@ data "aws_ami" "centos7" {
     values = ["ebs"]
   }
 }
+
+data "aws_ami" "debian9" {
+  owners      = ["679593333241"]
+  most_recent = "true"
+
+  filter {
+    name   = "name"
+    values = ["debian-stretch-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_ami" "rehl8" {
+  owners      = ["309956199498"]
+  most_recent = "true"
+
+  filter {
+    name   = "name"
+    values = ["RHEL-8.0.0*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_ami" "rehl7" {
+  owners      = ["309956199498"]
+  most_recent = "true"
+
+  filter {
+    name   = "name"
+    values = ["RHEL-7.7*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
