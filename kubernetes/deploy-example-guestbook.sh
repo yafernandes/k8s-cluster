07e2e2c0c7f0152092d5085d7fee3aee89e7aa23
yafernandes <alexandrino.fernandes@gmail.com>
2019-11-13 11:19:04 -0500

    Deployes Guestbook example

---
+++ kubernetes/deploy-example-guestbook.sh
@@ -0,0 +1,6 @@
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-master-deployment.yaml
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-master-service.yaml
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-slave-deployment.yaml
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-slave-service.yaml
+kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-deployment.yaml
+kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-service.yaml
