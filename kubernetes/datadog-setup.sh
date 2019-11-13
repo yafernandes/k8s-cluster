9aa0eeabca2529b5dcca659a884e7aa410d02c9f
yafernandes <alexandrino.fernandes@gmail.com>
2019-11-13 12:49:02 -0500

    Deploying kube-state-metrics from Github

--- kubernetes/datadog-setup.sh
+++ kubernetes/datadog-setup.sh
@@ -9,20 +9,15 @@ kubectl apply -f https://github.com/DataDog/datadog-agent/raw/master/Dockerfiles
 # https://docs.datadoghq.com/agent/kubernetes/event_collection/
 kubectl create configmap datadogtoken --from-literal="event.tokenKey"="0"
 
-kubectl create configmap dd-confd  --from-file dd-confd
-
 kubectl create secret generic dd --from-env-file=secrets.txt
 
-##https://docs.datadoghq.com/integrations/kubernetes/#kubernetes-state-metrics
-##https://docs.datadoghq.com/agent/kubernetes/host_setup/
-kubectl apply -f kube-state-metrics/examples/standard
-# kubectl apply -f metrics-server/deploy/1.8+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/service-account.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/cluster-role.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/cluster-role-binding.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/deployment.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/service.yaml
 
 kubectl apply -f datadog-config.yaml
 kubectl apply -f datadog-agent.yaml
 
 kubectl apply -f stresser.yaml
-
-##OpenShift
-# kubectl apply -f https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/openshift/scc.yaml
-# git clone https://github.com/kubernetes/kube-state-metrics.git
