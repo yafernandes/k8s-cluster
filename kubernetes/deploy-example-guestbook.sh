09edc74a41cdf8d7267aa3e1fae9554b731000aa
yafernandes <alexandrino.fernandes@gmail.com>
2019-11-14 08:56:00 -0500

    Adding ingress

--- kubernetes/deploy-example-guestbook.sh
+++ kubernetes/deploy-example-guestbook.sh
@@ -4,3 +4,5 @@ kubectl apply -f https://k8s.io/examples/application/guestbook/redis-slave-deplo
 kubectl apply -f https://k8s.io/examples/application/guestbook/redis-slave-service.yaml
 kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-deployment.yaml
 kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-service.yaml
+
+kubectl apply -f deploy-example-guestbook.yaml
