c532bfdcd06cf5f4de957cd09b3c8b8869caa4fb
yafernandes <alexandrino.fernandes@gmail.com>
2019-11-15 17:20:47 -0500

    Cleanup

--- kubernetes/datadog-setup.sh
+++ kubernetes/datadog-setup.sh
@@ -1,22 +1,12 @@
-##https://docs.datadoghq.com/agent/kubernetes/daemonset_setup/
-kubectl apply -f https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/clusterrole.yaml
-kubectl apply -f https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/serviceaccount.yaml
-kubectl apply -f https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/clusterrolebinding.yaml
-
-kubectl apply -f https://github.com/DataDog/datadog-agent/raw/master/Dockerfiles/manifests/cluster-agent/rbac/rbac-cluster-agent.yaml
-
-# Kubernetes Event Collection
-# https://docs.datadoghq.com/agent/kubernetes/event_collection/
-kubectl create configmap datadogtoken --from-literal="event.tokenKey"="0"
-
-kubectl create secret generic dd --from-env-file=secrets.txt
-
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/service-account.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/cluster-role.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/cluster-role-binding.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/deployment.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/service.yaml
 
+kubectl create secret generic dd --from-env-file=secrets.txt
+
+kubectl apply -f datadog-rbac.yaml
 kubectl apply -f datadog-config.yaml
 kubectl apply -f datadog-agent.yaml
 
