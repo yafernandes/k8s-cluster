1bc1ea55da5108bb698532ecb2350f1f5573cb52
yafernandes <alexandrino.fernandes@gmail.com>
2020-01-11 20:17:51 -0500

    Added Ubuntu images

--- terraform/amis.tf
+++ terraform/amis.tf
@@ -1,3 +1,53 @@
+data "aws_ami" "ubuntu_1910" {
+  owners      = ["aws-marketplace"]
+  most_recent = "true"
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-eoan-19.10-amd64-server*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_ami" "ubuntu_1910_min" {
+  owners      = ["aws-marketplace"]
+  most_recent = "true"
+
+  filter {
+    name   = "name"
+    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-eoan-19.10-amd64-minimal*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
 data "aws_ami" "centos7" {
   owners      = ["aws-marketplace"]
   most_recent = "true"
