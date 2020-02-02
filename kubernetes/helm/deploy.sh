332f2530b02cbf8e79c3b270a02d7159493cc718
yafernandes <alexandrino.fernandes@gmail.com>
2020-02-02 14:03:03 -0500

    Deploys Datadog on its own namepsace

--- kubernetes/helm/deploy.sh
+++ kubernetes/helm/deploy.sh
@@ -1,11 +1,11 @@
 # https://hub.helm.sh/charts/stable/datadog
 # https://github.com/helm/charts/tree/master/stable/datadog#configuration
 
-kubectl create secret generic dd --from-env-file=../secrets.txt
+kubectl create ns datadog
 
-# kubectl apply -f ../metrics-server/deploy/1.8+
+kubectl create secret generic dd --from-env-file=../secrets.txt -n datadog
 
-helm install datadog stable/datadog -f datadog-values.yaml
+helm install datadog stable/datadog -f datadog-values.yaml -n datadog
 
 kubectl apply -f nginx-configmap.yaml
 
@@ -13,8 +13,6 @@ helm install nginx stable/nginx-ingress -f nginx-values.yaml
 
 # helm install dashboard stable/kubernetes-dashboard -f dashboard-values.yaml
 kubectl apply -f dashboard-v2.yaml
-
-# helm upgrade -f datadog-values.yaml datadog stable/datadog --recreate-pods
 
 cat << EOF | kubectl apply -f -
 apiVersion: v1
