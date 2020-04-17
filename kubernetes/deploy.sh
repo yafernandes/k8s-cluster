9540cbfeb9f67967aab1e5c49290043443ed83f5
yafernandes <alexandrino.fernandes@gmail.com>
2020-04-17 10:14:40 -0400

    Deployment update

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -6,13 +6,3 @@ kubectl create ns datadog
 kubectl create secret generic dd --from-env-file=secrets.txt -n datadog
 
 helm install datadog stable/datadog -f datadog-values.yaml -n datadog
-
-kubectl create ns nginx-ingress
-
-kubectl apply -f nginx-configmap.yaml -n nginx-ingress
-
-helm install nginx-ingress stable/nginx-ingress -f nginx-values.yaml -n nginx-ingress
-
-kubectl apply -f stresser.yaml
-
-kubectl apply -f app-node.yaml
