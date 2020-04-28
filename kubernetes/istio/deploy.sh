976c620e27d0ae81ebf6881789f081654280d2b8
yafernandes <alexandrino.fernandes@gmail.com>
2020-04-27 20:31:02 -0400

    Limits deployment to just Istio

--- kubernetes/istio/deploy.sh
+++ kubernetes/istio/deploy.sh
@@ -2,8 +2,4 @@ kubectl label namespace default istio-injection=enabled
 
 istioctl manifest apply -f istio-config.yaml
 
-kubectl delete -f ../apps/app-node.yaml
-
-kubectl apply -f ../apps/app-node.yaml
-
 kubectl apply -f istio-ingress.yaml
