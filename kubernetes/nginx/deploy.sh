d5d8418c431571cf660fdd2d5f277edc53b2031b
yafernandes <alexandrino.fernandes@gmail.com>
2020-11-06 04:14:47 -0500

    NGINX deployment

--- kubernetes/nginx/deploy.sh
+++ kubernetes/nginx/deploy.sh
@@ -1,5 +1,3 @@
 kubectl create ns nginx-ingress
 
-kubectl apply -f nginx-configmap.yaml -n nginx-ingress
-
-helm install nginx-ingress stable/nginx-ingress -f nginx-values.yaml -n nginx-ingress
+helm install nginx-ingress nginx-stable/nginx-ingress -f nginx-values.yaml -n nginx-ingress
