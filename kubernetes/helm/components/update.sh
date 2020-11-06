4c962842279949f8ede7e8d1ffcc025de3a76f08
yafernandes <alexandrino.fernandes@gmail.com>
2020-11-06 05:42:51 -0500

    Added Istio 1.5+

--- kubernetes/helm/components/update.sh
+++ kubernetes/helm/components/update.sh
@@ -3,3 +3,4 @@ yq m -a append openshift4.yaml base.yaml > ../datadog-values-openshift4.yaml
 yq m -a append proxy.yaml base.yaml > ../datadog-values-proxy.yaml
 yq m -a append eks.yaml base.yaml > ../datadog-values-eks.yaml
 yq m -a append aks.yaml base.yaml > ../datadog-values-aks.yaml
+yq m -a append istio-1.5.yaml base.yaml > ../datadog-values-istio-1.5.yaml
