2118c806cbff67e60183a52d9f427f45cb1fb8e2
yafernandes <alexandrino.fernandes@gmail.com>
2020-11-06 12:05:30 -0500

    Better merging

--- kubernetes/helm/components/update.sh
+++ kubernetes/helm/components/update.sh
@@ -1,6 +1,6 @@
-yq m -a append etcd.yaml base.yaml > ../datadog-values.yaml
-yq m -a append openshift-4.yaml base.yaml > ../datadog-values-openshift-4.yaml
-yq m -a append proxy.yaml base.yaml > ../datadog-values-proxy.yaml
-yq m -a append eks.yaml base.yaml > ../datadog-values-eks.yaml
-yq m -a append aks.yaml base.yaml > ../datadog-values-aks.yaml
-yq m -a append istio-1.5.yaml base.yaml > ../datadog-values-istio-1.5.yaml
+yq m -xa append base.yaml etcd.yaml > ../datadog-values.yaml
+yq m -xa append base.yaml openshift-4.yaml > ../datadog-values-openshift-4.yaml
+yq m -xa append base.yaml proxy.yaml > ../datadog-values-proxy.yaml
+yq m -xa append base.yaml eks.yaml > ../datadog-values-eks.yaml
+yq m -xa append base.yaml aks.yaml > ../datadog-values-aks.yaml
+yq m -xa append base.yaml istio-1.5.yaml > ../datadog-values-istio-1.5.yaml
