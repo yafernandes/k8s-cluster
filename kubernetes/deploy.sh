2073016cd24d53b078b6ec9bcd86aa67e542b49d
yafernandes <alexandrino.fernandes@gmail.com>
2021-02-01 19:11:20 -0500

    Update

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,12 +1,7 @@
-# https://hub.helm.sh/charts/stable/datadog
-# https://github.com/helm/charts/tree/master/stable/datadog#configuration
-
-kubectl config current-context alex@kubernetes
-
 kubectl create ns datadog
 
 kubectl create secret generic datadog-keys --from-env-file=secrets.txt -n datadog
 
 kubectl create secret generic datadog-keys --from-env-file=secrets.txt
 
-helm install datadog datadog/datadog -f helm/datadog-values.yaml -n datadog
+helm install datadog datadog/datadog -n datadog -f $1
